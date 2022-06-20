30701df4b8d28afaada199b94dc1e7489f011a09
YULY-RAMIREZ <44952948+YULY-RAMIREZ@users.noreply.github.com>
2022-06-20 18:21:02 -0500

    Update 3. Importar y explorar datos.R

--- 3. Importar y explorar datos.R
+++ 3. Importar y explorar datos.R
@@ -17,18 +17,18 @@ rownames(datos)
 #Verificar el tipo de base de datos
 class(datos)
 
-#Dimensión de la base de datos
+#DimensiÃ³n de la base de datos
 dim(datos)
 
 #Inspeccionar datos faltantes
 is.na(datos)
 
 
-#Selecionar subconjunto de datos
-sub_conjunto <- datos[!is.na(datos$`Papers ISI 2015-2017`),]
+#Seleccionar subconjunto de datos
+sub_conjunto <- datos[!is.na(datos$$`AÃ±os con acreditaciÃ³n Joint Comission`),]
 dim(sub_conjunto)
 
-#Convertir variables caracter a numéricas
+#Convertir variables caracter a numÃ©ricas
 sub_conjunto[,2] <- sapply(sub_conjunto[, 2], as.numeric)
 
 #Configurar el nombre de las filas del conjunto de datos
@@ -41,40 +41,40 @@ sub_conjunto <- sub_conjunto[,-3]
 
 colnames(sub_conjunto)[5] <- c("Tipo de hospital")
 
-#Calcular la media de solo una columna de datos numéricos
+#Calcular la media de solo una columna de datos numÃ©ricos
 
 promedio <- mean(sub_conjunto[,14]) #promedio del Indicador seguridad
 promedio
 
-##Calcular la media de todas las columnas de datos numéricos
-promedio <- apply(as.matrix(sub_conjunto[,14:21]), 2, mean) ###el 2 índica que calcule lo que se le solicita por columnas,
-                                              #y en la mean, se puede colocar la función para calcular desviación,
+##Calcular la media de todas las columnas de datos numÃ©ricos
+promedio <- apply(as.matrix(sub_conjunto[,14:21]), 2, mean) ###el 2 Ã­ndica que calcule lo que se le solicita por columnas,
+                                              #y en la mean, se puede colocar la funciÃ³n para calcular desviaciÃ³n,
                                               #     y otros descriptivos
-promedio <- as.data.frame(promedio) #convertir la información de promedio a data frame
+promedio <- as.data.frame(promedio) #convertir la informaciÃ³n de promedio a data frame
 
 
-######Pueden aplicar la función anterior para calcular desviación, máximo, mínimo, entre otros.
+######Pueden aplicar la funciÃ³n anterior para calcular desviaciÃ³n, mÃ¡ximo, mÃ­nimo, entre otros.
 
 #Guardar las tablas en excel
 write.table(promedio,"promedio.txt") ##guarda en formato txt
 write.csv2(promedio,"promedio.csv") ##guarda en formato csv2 y lo abre en excel desde la carpeta en donde que da guardado
 
-###Verificar en dónde guarda los datos
-getwd() #Aquí muestra la ruta en dónde guarda los datos
+###Verificar en dÃ³nde guarda los datos
+getwd() #AquÃ­ muestra la ruta en dÃ³nde guarda los datos
 
 
 ##Diagrama de barras
 
-ggplot(sub_conjunto, aes(x=País, fill=País))+
+ggplot(sub_conjunto, aes(x=PaÃ­s, fill=PaÃ­s))+
   geom_bar(width = 1)+
   theme(legend.position = "none") +
   theme(text = element_text(size=10),
         axis.text.x = element_text(angle=90, hjust=1)) +
   ylab("Frecuencia")
 
-## Variable por país
-ggplot(sub_conjunto, aes(x = País, y = Seguridad))+
-  geom_boxplot(aes(fill=factor(País))) + 
+## Variable por paÃ­s
+ggplot(sub_conjunto, aes(x = PaÃ­s, y = Seguridad))+
+  geom_boxplot(aes(fill=factor(PaÃ­s))) + 
   theme_bw() + 
   theme(legend.position = "nonet") 
 
@@ -84,11 +84,11 @@ library(reshape2)
 
 
 
-datos_transf <- melt(sub_conjunto,id.vars=c('País','Ciudad','Tipo de hospital'), 
+datos_transf <- melt(sub_conjunto,id.vars=c('PaÃ­s','Ciudad','Tipo de hospital'), 
                      measure.vars=c("Seguridad", "Dignidad y Experiencia",
-                                    "Capital Humano", "Gestión del conocimiento",
+                                    "Capital Humano", "GestiÃ³n del conocimiento",
                                     "Capacidad","Eficiencia",
-                                    "Prestigio","Índice de calidad")) 
+                                    "Prestigio","Ãndice de calidad")) 
 
 
 
